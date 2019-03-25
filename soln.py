38a96c856c205192a22365694739de309f263066
hridayns <hridaysanghvi@gmail.com>
2019-03-25 15:26:04 +0530

    contour detection

--- soln.py
+++ soln.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 root = os.getcwd()
 data_folder = 'input'
 data_path = os.path.join(root,data_folder)
@@ -12,28 +13,51 @@ output_path = os.path.join(root,output_folder)
 if not os.path.exists(output_path):
 	os.makedirs(output_path)
 
+def draw(img,fn):
+	fig,ax = plt.subplots()
+	ax.axis('off')
+	ax.imshow(img,cmap='gray')
+	fig.savefig(os.path.join(output_path,fn),bbox_inches='tight')
+	plt.close()
+
+def draw_hist(data,fn):
+	fig,ax = plt.subplots()
+	(n,bins,patches) = ax.hist(data)
+	fig.savefig(os.path.join(output_path,'hist-'+fn),bbox_inches='tight')
+	plt.close()
+	print(n)
+	print(bins)
+	print(patches)
+
 for _,subfolders,files in os.walk(data_path):
 	for file in files:
 		print('------------------FILE {}--------------------'.format(file))
 		img_file = os.path.join(data_path,file)
+		if file == 'tiger.jpg':
+			continue
 		img = cv.imread(img_file)
+
 		gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-		edges = cv.Canny(gray_img,100,200)
 
-		op_plt,ax_op = plt.subplots()
-		ax_op.imshow(edges,cmap='gray')
-		op_plt.savefig(os.path.join(output_path,file),bbox_inches='tight')
-		plt.close()
+		gray_y = gray_img.flatten()
+		draw_hist(gray_y,file)
 
-		(_, contours,_) = cv.findContours(edges,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-		
-		img_w_contours = cv.drawContours(img, contours[0], -1, (0, 0, 255), 5)
+		blur_img = cv.GaussianBlur(gray_img, (5, 5), 0)
 
-		contour_plt,ax_con = plt.subplots()
-		ax_con.imshow(img_w_contours)
-		contour_plt.savefig(os.path.join(output_path,'contours-'+file),bbox_inches='tight')
-		plt.close()
+		# (t, binary_img) = cv.threshold(blur_img, 200, 255, cv.THRESH_BINARY)
+		# (t, binary_img) = cv.threshold(blur_img, 240, 255, cv.THRESH_BINARY)
+		(t, binary_img) = cv.threshold(blur_img,240,255, cv.THRESH_BINARY_INV)
+		# binary_img = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+
+		# draw(binary_img,file)
+
+		(_, contours, _) = cv.findContours(binary_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 		print('Found {} objects.'.format(len(contours)))
 		for (i, c) in enumerate(contours):
+
 			print('\tSize of contour {}: {}'.format(i, len(c)))
+
+		# contour_img = cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+		# draw(contour_img,file)
+		# edges = cv.Canny(gray_img,100,200)
